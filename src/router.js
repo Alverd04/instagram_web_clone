8de7d4b3513444aba9ac7379da37975e5ccab560
Alverd04 <albert.font.fernandez@gmail.com>
2022-01-25 20:46:02 +0100

    add shared page to new routes

--- src/router.js
+++ src/router.js
@@ -1,18 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import SharedPage from './components/SharedPage'
+import HomePage from './pages/HomePage'
 
 export default function RouteHandler() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route exact path="/:profile_id"></Route>
-        <Route exact path="/stories/:story_id"></Route>
-        <Route exact path="/messages"></Route>
-        <Route exact path="/publish"></Route>
-        <Route exact path="/discover"></Route>
-        <Route exact path="/likes"></Route>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route exact path='/:profile_id' element={<SharedPage />}></Route>
+        <Route exact path='/stories/:story_id' element={<SharedPage />}></Route>
+        <Route exact path='/messages' element={<SharedPage />}></Route>
+        <Route exact path='/publish' element={<SharedPage />}></Route>
+        <Route exact path='/discover' element={<SharedPage />}></Route>
+        <Route exact path='/likes' element={<SharedPage />}></Route>
       </Routes>
     </Router>
-  );
+  )
 }
